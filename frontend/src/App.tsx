f12e03f53918f51780f199f33c5c11ccd7a3a399
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-10-04 09:11:58 +0200

    Add login page to app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { NextUIProvider } from "@nextui-org/react";
 import InputComponents from "./components/molecules/input-components";
 import NavBar from "./components/molecules/navbar";
+import LoginPage from "./components/pages/login";
 
 function App() {
   return (
     <div className="App">
       <NextUIProvider>
         <NavBar />
+        <LoginPage />
       </NextUIProvider>
     </div>
   );
